b03ce57504bb66afaaa83f17bc2dfdbab3a0763a
Crio.GitLab <admin@example.com>
2020-05-12 17:26:56 +0530

    [ajay] updated TODOs

--- restaurants/facebook_post.py
+++ restaurants/facebook_post.py
@@ -19,35 +19,45 @@ class Facebook:
         self.page_id = Facebook.get_access_token('FACEBOOK_PAGE_ID')
         self.page_access_token = Facebook.get_access_token('FACEBOOK_PAGE_ACCESS_TOKEN')
 
-    #TODO: CRIO_TASK_MODULE_FACEBOOK_SHARE_FROM_CLI
+    # TODO: CRIO_TASK_MODULE_FACEBOOK_SHARE_FROM_CLI
     # Tasks:
     # 1) Complete the publish_photo_msg() function.
-    #    This function should publish a post with the given message and photo.
-    #    The post should be published to the QEats Facebook Page
-    #    You can use the page ID and access tokens provided in self.page_id and self.page_access_token
-    # 2) To find which API to hit, check out developer section of the Facebook API (https://developers.facebook.com/docs/graph-api/reference/page/photos/#publish)
-    # 3) To know how to make an API request in Python have a look at - https://crio-assist.zendesk.com/hc/en-us/articles/360038676513-How-to-make-an-API-request-in-Python
-
+    #    - This function should publish a post with the given message and photo.
+    #    - The post should be published to the QEats Facebook Page.
+    #
+    # 2) Don't hardcode any values.
+    #    - Our assessment servers might use your `publish_photo_msg` function
+    #      to post to a different FB page.
+    #    - If you hardcode the page ID or access token, the assessment might
+    #      fail.
+    #    - Tip: use self.page_id and self.page_access_token
+    #
+    # 3) Wondering which Facebook REST API endpoint to use? Check this:
+    #    - https://developers.facebook.com/docs/graph-api/reference/page/photos/#publish
+    # 
+    # 4) Want to learn more about making HTTP API requests using Python?
+    #    - https://forum.crio.do/t/how-can-i-make-http-requests-using-python/12659
 
     # Parameters
     # ----------
-    # message : string
-    #     message to be posted
-    # image_url : string
-    #     publicly accessible URL of the image to be posted
+    # message : string (message to be posted)
+    # image_url : string (publicly accessible URL of the image to be posted)
     # Return Type: None
+    
     def publish_photo_msg(self, message, image_url):
         # write your code here
         return
 
 if __name__ == '__main__':
     facebook = Facebook()
+    
     # TODO: CRIO_TASK_MODULE_FACEBOOK_SHARE_FROM_CLI
     # Tasks:
     # 1) Search for your favorite ice-cream picture on images.google.com
     # 2) Copy the URL of the image and assign it to the 'image_url' variable
-    #    Eg: image_url = 'http://ksmartstatic.sify.com/cmf-1.0.0/appflow/bawarchi.com/Image/oeturjecjjdah_bigger.jpg'
-    # 3) Fill the 'my_name' variable with your name so that you know the posts you have created
+    #    Eg: image_url = 'https://assetsds.cdnedge.bluemix.net/sites/default/files/styles/very_big_1/public/feature/images/ice_cream_1.jpg'
+    # 3) Set the 'my_name' variable to your name so you can identify your posts.
+    
     image_url = ''
     my_name = ''
 
